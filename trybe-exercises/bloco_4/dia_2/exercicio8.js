8757c58bca6200e40aef50d4690e38e199f199c4
Fabio <fabiohigoralm2@gmail.com>
2020-10-29 00:18:07 -0300

    ultimo commit bloco 4 dia 3

---
+++ trybe-exercises/bloco_4/dia_2/exercicio8.js
@@ -0,0 +1,7 @@
+let array=[]
+let soma = []
+for (let i= 1 ;i<=25 ; i++){
+ soma.push(i)
+   
+}
+console.log(soma)
---
+++ trybe-exercises/bloco_4/dia_2/exercicio9.js
@@ -0,0 +1,7 @@
+let array=[]
+let soma = []
+for (let i= 1 ;i<=25 ; i++){
+ soma.push(i/2)
+   
+}
+console.log(soma)
